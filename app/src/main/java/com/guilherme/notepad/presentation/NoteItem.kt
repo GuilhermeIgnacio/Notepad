992afaa357cd3ec311beb1c5e90fbfed1bfb32e2
Guilherme Ignácio <stormierearth9@gmail.com>
2024-03-17 15:48:44 -0300

    formated body

--- app/src/main/java/com/guilherme/notepad/presentation/NoteItem.kt
+++ app/src/main/java/com/guilherme/notepad/presentation/NoteItem.kt
@@ -33,11 +33,15 @@ import com.guilherme.notepad.R
 import com.guilherme.notepad.data.NoteEvents
 import com.guilherme.notepad.data.NoteState
 import com.guilherme.notepad.models.Note
+import com.mohamedrejeb.richeditor.model.RichTextState
+import com.mohamedrejeb.richeditor.model.rememberRichTextState
+import com.mohamedrejeb.richeditor.ui.material3.RichText
+import com.mohamedrejeb.richeditor.ui.material3.RichTextEditor
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalFoundationApi::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NoteItem(
@@ -116,11 +120,14 @@ fun Item(
                 )
             }
 
-            Text(
-                text = note.noteBody ?: "",
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Normal
+            val richTextState = rememberRichTextState()
+            richTextState.setHtml(note.noteBody ?: "")
+            RichText(
+                state = richTextState,
+                fontSize = 16.sp
             )
+
+
 
             val date = LocalDateTime.parse(
                 note.noteLastChange
